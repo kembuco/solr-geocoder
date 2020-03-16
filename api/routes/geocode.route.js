f0804813057111cf249e4b809f267c3ce0794e77
Kevin Burke <kembuco@gmail.com>
2020-03-16 10:41:33 -0700

    Adding a query builder.

--- api/routes/geocode.route.js
+++ api/routes/geocode.route.js
@@ -4,18 +4,28 @@ const axios = require('axios').create({
   baseURL: process.env.SOLR_URL
 });
 
-module.exports = async function (fastify, options) {  
-  fastify.get('/geocode/:address', async ({ params }, reply) => {
+module.exports = async function (fastify, options) {
+  async function geocode({ address }, reply){
     // First, try to geocode the address string
-    let geocode = await axios.get('/select', { params: toParams(params.address) });
+    let geocode = await axios.get('/select', { params: toParams(address) });
 
     // Second, parse the address into it's parts for a more expansive search
     if ( !geocode.data.response.numFound ) {
-      const parsed = await parseAddress(params.address);
+      const parsed = await parseAddress(address);
       
       geocode = await axios.get('/select', { params: toParams(parsed) });
     }
     
     return geocode.data;
+  }
+
+  fastify.get('/geocode/:address', async ({ params }, reply) => {
+    const { response } = await geocode(params);
+
+    return response;
+  });
+  
+  fastify.get('/geocode/debug/:address', async ({ params }, reply) => {
+    return await geocode(params);
   });
 }
