ca23af17821e73a4daf1b9a4ecc73f9fe6fde508
Kevin Burke <kevin.burke@state.co.us>
2020-04-02 17:27:11 -0600

    Adding a parsing endpoint for convenience and fun. And just because.

--- api/src/routes/v1/geocoding.routes.js
+++ api/src/routes/v1/geocoding.routes.js
@@ -3,10 +3,17 @@ const {
   reverseQuery,
   batchQuery
 } = require('../../services/geocoding.service');
+const {
+  parseAddress
+} = require('../../services/address-parsing.service');
 
 module.exports = async function (fastify) {
   fastify.get('/geocode/forward/:address', async ({ params }) => {
     return await forwardQuery(params.address);
+  });
+
+  fastify.get('/geocode/parse/:address', async ({ params }) => {
+    return await parseAddress(params.address);
   });
 
   fastify.get('/geocode/reverse/:latlon', async ({ params }) => {
