92e3441f3eff1cc9b70c167c0666d6c6272bf767
Kevin Burke <kevin.burke@state.co.us>
2020-04-09 07:04:18 -0600

    Adding missing files.

---
+++ api/src/providers/solr/round-robin.js
@@ -0,0 +1,23 @@
+let index = 0;
+let pool;
+
+/**
+ * A very simplistic function that chooses the next item in the pool. If
+ * the end of the pool has been reached, it starts again from the beginning.
+ * 
+ * @param {Array} candidates A list of candidates to choose from
+ */
+function roundRobin( candidates = []) {
+  pool = candidates;
+
+  return () => {
+    if ( index == pool.length - 1 ) {
+      index = 0;
+    } else {
+      index = index +1;
+    }
+
+    return pool[index];
+  }
+}
+module.exports = roundRobin;
