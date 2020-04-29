d8a6e3feb3056e00e4571e86e0e8fcf2a68ca1ae
Kevin Burke <kevin.burke@state.co.us>
2020-04-29 09:34:40 -0600

    Adding better startup and shutdown helpers.

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 zookeeper/bin/zkServer.sh start
 
 solr/bin/solr start -c -s solr/node1/solr -p 8983 -z 127.0.0.1:2181
 solr/bin/solr start -c -s solr/node2/solr -p 8984 -z 127.0.0.1:2181
 solr/bin/solr start -c -s solr/node3/solr -p 8985 -z 127.0.0.1:2181
-
-cd api && yarn api:start && cd ..
--- stop.sh
+++ stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 zookeeper/bin/zkServer.sh stop
 solr/bin/solr stop -all
-cd api && yarn api:stop && cd ..
